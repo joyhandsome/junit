363ddc2d12c2d8ddd68510f2aaaaa54ff2c8ca7a
wang-junhao <wang.junhao@navercorp.com>
2020-09-24 11:14:33 +0800

    project init

---
+++ src/test/java/com/wang/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
